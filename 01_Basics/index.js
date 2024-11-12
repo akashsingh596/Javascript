16bdf78f431af882868f23ae3bafcf2b89062a21
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-12 21:26:26 +0530

    update

---
+++ 01_Basics/index.js
@@ -0,0 +1,7 @@
+const accountID = 61725
+let accountEmail ="jashfdj@gmail.com"
+var accountPass= "1235566"
+
+accountCity = "jaipur"
+
+console.table([accountID,accountEmail ,accountPass ,accountCity])
