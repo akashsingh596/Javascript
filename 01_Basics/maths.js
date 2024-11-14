3dc25cf3e77ec7e8f177d59d6fd510df9e9435ff
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-14 14:21:04 +0530

    numsandmaths

---
+++ 01_Basics/maths.js
@@ -0,0 +1,28 @@
+const score = 400;
+// console.log(score);
+const balance = new Number(100);
+
+// console.log(balance);
+// console.log(balance.toString());
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.895932;
+
+// console.log(otherNumber.toPrecision(5));
+
+const hundrends = 1000000000;
+
+// console.log(hundrends.toLocaleString("en-IN"));
+
+/////////////////maths///////////////
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) +min));
