e932fbf80aa5b305c0e21dfce71842f4be646e66
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2024-11-14 17:24:57 +0530

    date_time

---
+++ 01_Basics/date_and_time.js
@@ -0,0 +1,19 @@
+console.log("hello");
+let mydate = new Date();
+
+
+
+
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toJSON());
+
+console.log(mydate.toLocaleString());
+
+let mycreateddate = new Date(2023,0 ,23)
+
+console.log(mycreateddate.toLocaleString())
+
+let mytimestamp =  Date.now()
+
+console.log(mytimestamp)
